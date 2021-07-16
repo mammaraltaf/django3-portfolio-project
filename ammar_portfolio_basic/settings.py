73b0960b04612126a890a4384d4f116507a2ad3a
mammaraltaf <mammaraltaf@gmail.com>
2021-07-16 20:25:40 +0000

    Removed old stuff

--- ammar_portfolio_basic/settings.py
+++ ammar_portfolio_basic/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qy8un!xf=xxi#di675mx6vs(s15@qtr3&w%2r!^ca*h$g*3x)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mammaraltaf.pythonanywhere.com']
 
 
 # Application definition
@@ -121,5 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
